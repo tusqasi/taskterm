1618a9bb3c520aab661803f736ffd7582e9a52a5
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-07-03 08:54:12 +0530

    made a cond for 'a'

--- run.py
+++ run.py
@@ -5,5 +5,15 @@ tasks = []
 
 while True:
 	if c == 0:
-		prmt = str(input("'a' for adding new task\n'l' for listing all tasks\n
-			             'c' for changing a task")) 
+		prmt = str(input("'a' for adding new task\n'l' for listing all tasks\n'c' for changing a task\n")) 
+	
+	else:
+		prmt = str(input("'l' for listing all tasks\n'c' for changing a task"))	
+
+
+	if prmt == 'a':
+		c+=1
+		usrn, usrdu ,usrds = str(input("name:duration:description (separated by colons)")) 
+		tasks.append(Task(name=usrn, dur=usrdu, desc=usrds, ind=c))
+
+	
