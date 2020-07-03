15686e92134cfd57a96e636f5ec27d977c2a1ce4
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-07-03 08:43:46 +0530

    in run.py made basic additions like a loop and taking input

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from defs import Task
 
+c = 0
+tasks = []
+
+while True:
+	if c == 0:
+		prmt = str(input("'a' for adding new task\n'l' for listing all tasks\n
+			             'c' for changing a task")) 
